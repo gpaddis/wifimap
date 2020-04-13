a6bffdae399f0ff1d2c57dfcb361ad53a9b56938
Gianpiero Addis <gianpiero.addis@gmail.com>
2020-04-13 12:26:03 +0200

    Extract parser class

--- lib/wifimap.rb
+++ lib/wifimap.rb
@@ -1,12 +1,8 @@
-require "wifimap/version"
+# frozen_string_literal: true
+
+require 'wifimap/parser'
+require 'wifimap/version'
 
 module Wifimap
   class Error < StandardError; end
-  # Your code goes here...
-
-  class Parser
-    def self.parse(file_content)
-      []
-    end
-  end
 end
---
+++ lib/wifimap/parser.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Wifimap
+  class Parser
+    # Parse the content of a dump file and return an array of wifimap objects.
+    def self.parse(_file_content)
+      []
+    end
+  end
+end
