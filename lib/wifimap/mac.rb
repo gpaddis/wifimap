ec1cf67e50ef11480d7f8f0699b4ce139df4dc11
Gianpiero Addis <gianpiero.addis@sumcumo.com>
2020-04-13 16:42:07 +0200

    Get the manufacturer of a mac

--- lib/wifimap/mac.rb
+++ lib/wifimap/mac.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
+require 'louis'
+
 module Wifimap
   class Mac
     # Return true if the mac format is correct, false otherwise.
     def self.valid?(mac)
       mac =~ /([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})/ ? true : false
+    end
+
+    # Get the manufacturer for the given mac address.
+    def self.manufacturer(mac)
+      Louis.lookup(mac)['long_vendor']
     end
   end
 end
