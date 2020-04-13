bdc1dc87b9684879a230f1d0dc079f3e772af09b
Gianpiero Addis <gianpiero.addis@gmail.com>
2020-04-13 12:37:56 +0200

    Check for valid file content

--- lib/wifimap/parser.rb
+++ lib/wifimap/parser.rb
@@ -6,5 +6,12 @@ module Wifimap
     def self.parse(_file_content)
       []
     end
+
+    def self.dump_format(file_content)
+      # TODO: perform thorough checks on file content.
+      raise Error unless file_content.include?('BSSID')
+
+      'airodump'
+    end
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -5,3 +5,15 @@ RSpec.describe Wifimap::Parser, '.parse' do
     expect(Wifimap::Parser.parse(airodump_file)).to be_an_instance_of(Array)
   end
 end
+
+RSpec.describe Wifimap::Parser, '.dump_format' do
+  let(:airodump_file) { File.read('spec/files/airodump.csv') }
+
+  it 'identifies a valid airodump format' do
+    expect(Wifimap::Parser.dump_format(airodump_file)).to eq('airodump')
+  end
+
+  it 'raises an error if the dump format is unknown' do
+    expect { Wifimap::Parser.dump_format('invalid file content') }.to raise_error
+  end
+end
