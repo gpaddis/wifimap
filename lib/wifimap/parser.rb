557fb3485fd4f53b3770bc9cd00db84200e9cdfd
Gianpiero Addis <gianpiero.addis@gmail.com>
2020-04-18 10:36:16 +0200

    Define private class methods in a block

--- lib/wifimap/parser.rb
+++ lib/wifimap/parser.rb
@@ -10,16 +10,19 @@ module Wifimap
       return :sniff_probes if sniff_probes_format?(file_content)
     end
 
-    private
-    def self.airodump_format?(file_content)
-      airodump_header = 'BSSID, First time seen, Last time seen, channel, Speed, Privacy, Cipher, Authentication, Power'
-      file_content.include?(airodump_header)
-    end
+    class << self
+      private
 
-    def self.sniff_probes_format?(file_content)
-      file_content.split('\n').all? do |row|
-        fields = row.split
-        fields[1].include?('dBm') && Wifimap::Mac.valid?(fields[2])
+      def airodump_format?(file_content)
+        airodump_header = 'BSSID, First time seen, Last time seen, channel, Speed, Privacy, Cipher, Authentication, Power'
+        file_content.include?(airodump_header)
+      end
+
+      def sniff_probes_format?(file_content)
+        file_content.split('\n').all? do |row|
+          fields = row.split
+          fields[1].include?('dBm') && Wifimap::Mac.valid?(fields[2])
+        end
       end
     end
   end
