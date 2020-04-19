ece29704b4e1c10a6d2500b5a3706ba1639ec47d
Gianpiero Addis <gianpiero.addis@sumcumo.com>
2020-04-19 12:21:22 +0200

    Refactor to ParserFactory class

--- lib/wifimap.rb
+++ lib/wifimap.rb
@@ -1,23 +1,14 @@
 # frozen_string_literal: true
 
 require 'wifimap/version'
-require 'wifimap/parser'
-require 'wifimap/parser/airodump'
-require 'wifimap/parser/sniff_probes'
+require 'wifimap/parser_factory'
 
 module Wifimap
   class Error < StandardError; end
 
-  # Parse the content of a dump file and return a hash of APs, stations and probes.
+  # Parse the content of a dump file and return a parser instance.
   def self.parse(file_content)
-    case Wifimap::Parser.dump_format(file_content)
-    when :airodump
-      Wifimap::Parser::Airodump.new(file_content)
-    when :sniff_probes
-      Wifimap::Parser::SniffProbes.new(file_content)
-    else
-      raise Error, 'Unsupported dump format'
-    end
+    Wifimap::ParserFactory.create(file_content)
   end
 
   # A wrapper on .parse to read directly from a file.
