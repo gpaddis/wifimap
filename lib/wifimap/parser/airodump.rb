bff20e5e35e619303c12d0e9b0a4dc4095c8b0e7
Gianpiero Addis <gianpiero.addis@gmail.com>
2020-06-14 00:17:19 +0200

    Remove redundant split

--- lib/wifimap/parser/airodump.rb
+++ lib/wifimap/parser/airodump.rb
@@ -14,14 +14,13 @@ module Wifimap
       #
       # @return [Array] of Wifimap::AccessPoint
       def access_points
-        aps = rows.each.filter do |row|
-          fields = row.split(',')
+        aps = rows.each.map { |row| row.split(',') }
+        aps.filter! do |fields|
           channel = fields[3].to_i
           Mac.valid?(fields.first) && channel.between?(1, 16)
         end
 
-        aps.map do |ap|
-          fields = ap.split(',')
+        aps.map do |fields|
           AccessPoint.new(
             bssid: fields.first,
             privacy: fields[5].strip,
@@ -34,19 +33,16 @@ module Wifimap
       #
       # @return [Array] of Wifimap::Station
       def stations
-        stations = rows.select do |row|
-          fields = row.split(',')
+        stations = rows.each.map { |row| row.split(',') }
+        stations.filter! do |fields|
           power = fields[3].to_i
           Mac.valid?(fields.first) && power.negative?
         end
 
-        stations.map do |row|
-          fields = row.split(',')
+        stations.map do |fields|
           station = Station.new(mac: fields.first)
           station.associations << fields[5].strip unless fields[5].include?('(not associated)')
-
           station.probes << fields[6].strip unless fields[6].strip.empty?
-
           station
         end
       end
